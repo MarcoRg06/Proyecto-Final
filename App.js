1d170d52cf21f2f2557645ceadb220443222f472
MarcoRg06 <22620119@tlaxiaco.tecnm.mx>
2025-09-27 00:18:26 -0600

    subiendo cambios en app.js y los archivos .json

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//imprimir las pantallas
+import PantallaInicio from "./src/pages/Pantalla_Inicio/PantallaInicio";
+import ResgistrarAlunmos from "./src/pages/Pantalla_Peticiones/RegistrarAlumnos";
+import ListarAlumno from "./src/pages/Pantalla_Peticiones/ListarAlumno";
+import Actualizar from "./src/pages/Pantalla_Peticiones/Actualizar";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="PantallaInicio"  
+        screenOptions={{ headerShown: false }}>
+         <Stack.Screen name="PantallaInicio" component={PantallaInicio} />
+        <Stack.Screen name="RegistrarAlumnos" component={ResgistrarAlunmos} />
+         <Stack.Screen name="ListarAlumno" component={ListarAlumno} />
+         <Stack.Screen name="Actualizar" component={Actualizar} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
